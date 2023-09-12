1baef442cc416981d68b7c6835f41b5a09c59f04
memory <memory@qq.com>
2023-09-13 00:00:16 +0800

    成为好友，数据库信息完整性 加入队伍完成 展示已加入的队伍、我创建的队伍 展示队长昵称

--- src/components/userInfoPage.vue
+++ src/components/userInfoPage.vue
@@ -1,8 +1,69 @@
 <template>
   <div>
     <a-tabs v-model:activeKey="activeKey" tab-position="left" @change="handleTabChange">
-      <a-tab-pane key="1" tab="已加入的队伍">已加入的队伍</a-tab-pane>
-      <a-tab-pane key="2" tab="我创建的队伍">我创建的队伍</a-tab-pane>
+      <a-tab-pane key="1" tab="已加入的队伍">
+        <a-list
+            item-layout="horizontal"
+            :data-source="joinedTeamList"
+        >
+          <template #renderItem="{ item }">
+            <a-list-item>
+              <template #actions>
+                <!--队长-->
+                <span>队长</span>
+                <a-tag color="success">
+                  {{ item.userName }}
+                </a-tag>
+                <a-button size="large" type="primary" ghost>队内聊天</a-button>
+              </template>
+
+              <a-list-item-meta
+                  :description="item.description"
+              >
+                <template #title>
+                  <span>{{ item.name }}</span>
+                </template>
+
+                <template #avatar>
+                  <a-avatar :src="item.imgUrl"/>
+                </template>
+              </a-list-item-meta>
+            </a-list-item>
+          </template>
+        </a-list>
+      </a-tab-pane>
+
+      <a-tab-pane key="2" tab="我创建的队伍">
+        <a-list
+            item-layout="horizontal"
+            :data-source="createdTeamList"
+        >
+          <template #renderItem="{ item }">
+            <a-list-item>
+              <template #actions>
+                <!--队长-->
+                <span>队长</span>
+                <a-tag color="success">
+                  {{ item.userName }}
+                </a-tag>
+                <a-button size="large" type="primary" ghost>队内聊天</a-button>
+              </template>
+              <a-list-item-meta
+                  :description="item.description"
+              >
+                <template #title>
+                  <span>{{ item.name }}</span>
+                </template>
+
+                <template #avatar>
+                  <a-avatar :src="item.imgUrl"/>
+                </template>
+              </a-list-item-meta>
+            </a-list-item>
+          </template>
+        </a-list>
+      </a-tab-pane>
+
       <a-tab-pane key="3" tab="我的好友">
         <a-list
             item-layout="horizontal"
@@ -11,10 +72,10 @@
           <template #renderItem="{ item }">
             <a-list-item>
               <template #actions>
-                <a-button size="large" type="primary" ghost>聊天</a-button>
+                <a-button size="large" type="primary" ghost>私聊</a-button>
               </template>
               <a-list-item-meta
-                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                  :description="item.profile"
               >
                 <template #title>
                   <span>{{ item.username }}</span>
@@ -26,18 +87,16 @@
               </a-list-item-meta>
 
               <div v-if="item.isOnline === 1">
-                <a-tag color="processing" >
+                <a-tag color="processing">
                   在线
                 </a-tag>
               </div>
 
               <div v-if="item.isOnline === 0">
-                <a-tag color="default" >
+                <a-tag color="default">
                   下线
                 </a-tag>
               </div>
-
-
             </a-list-item>
           </template>
         </a-list>
@@ -51,9 +110,14 @@
 <script setup lang="ts">
 import {withDefaults, defineProps, ref} from "vue";
 import myAxios from "@/plugins/myAxios";
+import currentUser from "@/model/currentUser";
 
 // 好友列表
 const friendList = ref();
+// 我创建的队伍
+const createdTeamList = ref();
+// 我加入的队伍
+const joinedTeamList = ref();
 
 interface Props {
   userInfo: any[];
@@ -67,6 +131,12 @@ const props = withDefaults(defineProps<Props>(), {
 // 监听Tab标签变化
 const handleTabChange = (key: any) => {
   // 根据选项卡的key发送不同的请求
+  if (key === "1") {
+    getJoinedTeam();
+  }
+  if (key === "2") {
+    getCreatedTeam();
+  }
   if (key === "3") {
     getFriendList();
   }
@@ -76,6 +146,34 @@ const handleTabChange = (key: any) => {
 const getFriendList = () => {
   myAxios.get("/friends/list",).then((res) => {
     friendList.value = res.data.records;
+  }).catch(() => {
+    console.log("加入失败")
+  });
+};
+
+// 登录用户id
+const userId = currentUser.value.id;
+// 申请添加好友
+const getCreatedTeam = () => {
+  myAxios.get("/team/created", {
+    params: {
+      "loginUserId": userId
+    }
+  }).then((res) => {
+    createdTeamList.value = res.data.records;
+  }).catch(() => {
+    console.log("加入失败")
+  });
+};
+
+// 申请添加好友
+const getJoinedTeam = () => {
+  myAxios.get("/team/joined", {
+    params: {
+      "loginUserId": userId
+    }
+  }).then((res) => {
+    joinedTeamList.value = res.data.records;
   }).catch(() => {
     console.log("加入失败")
   });
